4832a3a53c731d9be54cd402d14bcc22bac782fd
Sahan Weerakkody <it22143068@my.sliit.lk>
2023-11-19 21:48:50 +0530

    edit comment

--- admin/main.js
+++ admin/main.js
@@ -10,7 +10,7 @@ function activeLink() {
 
 list.forEach((item) => item.addEventListener("mouseover", activeLink));
 
-// Menu Toggle
+// Menu Togglefreer
 let toggle = document.querySelector(".toggle");
 let navigation = document.querySelector(".navigation");
 let main = document.querySelector(".main");
